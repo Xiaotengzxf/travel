0376a1b76712018cfc1524a094c0c2d0d5184237
bruce.zhang <315082431@qq.com>
2018-08-29 21:15:48 +0800

    2018-08-29

---
+++ Travel/View/Main/Mine/JAccountSettingsViewController.swift
@@ -0,0 +1,63 @@
+//
+//  JAccountSettingsViewController.swift
+//  Travel
+//
+//  Created by ANKER on 2018/8/28.
+//  Copyright © 2018年 team. All rights reserved.
+//
+
+import UIKit
+
+class JAccountSettingsViewController: SCBaseViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func save(_ sender: Any) {
+    }
+}
+
+extension JAccountSettingsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kCell, for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 62
+        } else {
+            return 52
+        }
+    }
+}
